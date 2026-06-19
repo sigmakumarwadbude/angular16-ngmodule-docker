f9fb7ba09fa3381e6e75329b7a8aef8cb1007fa6
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-06-19 17:20:03 +0530

    feat: add StarComponent and integrate Font Awesome for star ratings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { WelcomeComponent } from './features/home/welcome.component';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './features/products/product-detail/product-detail.component';
+import { StarComponent } from './shared/star/star.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, ProductListComponent, ProductDetailComponent],
+  declarations: [AppComponent, WelcomeComponent, ProductListComponent, ProductDetailComponent, StarComponent],
   imports: [BrowserModule, CommonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
