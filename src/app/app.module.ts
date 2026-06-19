ecff91f6465e1731a5f3b9ad2ac8c3e863acae08
Sigma Kumar Wadbude <sigmawadbude@gmail.com>
2026-06-19 12:49:19 +0530

    feat(product): add ProductList component with mock product data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './features/home/welcome.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, WelcomeComponent, ProductListComponent],
+  imports: [BrowserModule, CommonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
